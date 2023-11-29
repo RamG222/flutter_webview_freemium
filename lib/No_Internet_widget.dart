aa45662c5cf882fb470a3da3f31ecb08370b7dbc
Ram <shriramgupta8141@gmail.com>
2023-11-29 11:16:27 +0530

    made changes to no internet widget

--- lib/No_Internet_widget.dart
+++ lib/No_Internet_widget.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: file_names
 
+import 'package:asdf/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:restart_app/restart_app.dart';
 
 class NoInternetWidget extends StatelessWidget {
   const NoInternetWidget({
@@ -10,8 +12,33 @@ class NoInternetWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //returns a widget when there is no internet conncetion
-    return const Center(
-      child: Text('NO INTERNET CONNCETION'),
+    return Center(
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Container(
+          height: 250,
+          child: Image.asset('assets/no_internet.gif'),
+        ),
+        SizedBox(height: 20),
+        Text('NO INTERNET CONNCETION'),
+        SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 35),
+          child: FilledButton.tonal(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Homepage()));
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.restart_alt_rounded),
+                SizedBox(width: 10),
+                Text('Retry')
+              ],
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
